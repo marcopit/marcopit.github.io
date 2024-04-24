f2c88a2b63e600a8aabe00d46fbe15c6c4b531d6
marcopit <marcopit94@gmail.com>
2024-04-24 20:41:49 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function calcola() {
+    var num1 = parseFloat(document.getElementById('input1').value);
+    var num2 = parseFloat(document.getElementById('input2').value);
+    var num3 = parseFloat(document.getElementById('input3').value);
+    
+    // Esempio di una semplice funzione matematica (somma dei numeri)
+    var risultato = num1 + num2 + num3;
+    
+    document.getElementById('risultato').innerHTML = "Il risultato è: " + risultato;
+}
